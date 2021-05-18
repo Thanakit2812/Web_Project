5b6dcb92c60fd7ea404664152eb341aec781d03c
tamtemza <tamtemza@gmail.com>
2021-05-18 21:30:17 +0700

    sss

--- teacher/checklogin.php
+++ teacher/checklogin.php
@@ -7,7 +7,6 @@ include("../database/database.php");
     $query = "SELECT * FROM teacher WHERE teachercode  = '$username' AND password = '$password'";
     $result = mysqli_query($conn, $query);
     if (mysqli_num_rows($result) == 1) {
-        // $objResult = mysqli_fetch_array($result);
         $_SESSION['teachercode'] = $username ;
         setcookie("cookieteachercode","$username", time() + 3600);
         header("location: home_teacher.html");
