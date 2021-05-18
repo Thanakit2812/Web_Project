33ae744628d0cb2177962f162bf08674c62725d0
tamtemza <tamtemza@gmail.com>
2021-05-18 18:22:43 +0700

    update

--- student/subject1.php
+++ student/subject1.php
@@ -86,9 +86,8 @@ $objResult2 = mysqli_fetch_array($result2);
             <th width="10%" scope="col">#</th>
             <th width="15%" scope="col">Subject ID</th>
             <th width="25%" scope="col">Subject</th>
-            <!-- <th width="15%" scope="col">Credit</th> -->
+            <th width="15%" scope="col">Credit</th>
             <th width="35%" scope="col">Teacher</th>
-            <th width="35%" scope="col">Teacher1</th>
           </tr>
         </thead>
         <tbody>
