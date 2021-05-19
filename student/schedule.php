307e574882d5c44e986c950f74b89cfa5501022a
MeteePoyoi <MeteePoyoi>
2021-05-19 23:28:54 +0700

    table2

--- student/schedule.php
+++ student/schedule.php
@@ -93,7 +93,6 @@
                   <th width="10%" scope="col">#</th>
                   <th width="15%" scope="col">Subject ID</th>
                   <th width="25%" scope="col">Subject</th>
-                  <th width="15%" scope="col">Credit</th>
                   <th width="35%" scope="col">Teacher</th>
               </tr>
           </thead>
@@ -123,12 +122,5 @@
   </div>
   </div>
 </nav>
-  
-<div class="container">
-  <h3>Collapsible Navbar</h3>
-  <p>In this example, the navigation bar is hidden on small screens and replaced by a button in the top right corner (try to re-size this window).
-  <p>Only when the button is clicked, the navigation bar will be displayed.</p>
-</div>
-
 </body>
 </html>
